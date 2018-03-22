9c68d805714b7291ccff75c9881ade6a4182cfb4
Averin Dmitry <averin@intaro.email>
2018-03-22 08:40:12 +0300

    k2_les2 end

---
+++ local/php_interface/include/old_actions_agent.php
@@ -0,0 +1,61 @@
+<?
+function oldActionCheck()
+{
+	if (CModule::IncludeModule("iblock"))
+	{
+
+		//query
+		$arSelect = Array("ID", "NAME", "DATE_ACTIVE_TO");
+		$arFilter = Array("IBLOCK_ID" => IBCLOCK_ACTIONS_ID, "!ACTIVE_DATE" => "Y", "ACTIVE" => "Y");
+		$res = CIBlockElement::GetList('', $arFilter, '', '', $arSelect);
+
+		//calc
+		$count = 0;
+		while ($ob = $res->GetNextElement('', false))
+			$count++;
+
+
+		if ($count)
+		{
+			//write to log
+			CEventLog::Add(array("SEVERITY"      => "SECURITY",
+			                     "AUDIT_TYPE_ID" => "OLD_ACTIONS",
+			                     "MODULE_ID"     => "iblock",
+			                     "ITEM_ID"       => 'iblock:actions',
+			                     "DESCRIPTION"   => "Устаревшие, но еще активные акции: " . $count . " шт.",));
+
+			//get groups
+			$gid = [];
+			$filter = Array("ACTIVE" => "Y",
+			                "ADMIN"  => "Y");
+			$rsGroups = CGroup::GetList(($by = "id"), ($order = "asc"), $filter);
+			while ($group =& $rsGroups->GetNext('', false))
+				$gid[] = $group['ID'];
+
+			//get admin list
+			$admins = [];
+			foreach ($gid as $id)
+				$admins = array_merge($admins, CGroup::GetGroupUser($id));
+
+			//get emails
+			$emails = [];
+			foreach ($admins as $userId)
+			{
+				$emails[] = CUser::GetByID($userId)->Fetch()['EMAIL'];
+			}
+
+			//send emails
+			if (count($emails))
+			{
+				$arEventFields = array("EMAIL_TO" => implode(",", $emails),
+				                       "COUNT"    => $count,);
+				CEvent::Send("OLD_ACTIONS", SITE_ID, $arEventFields);
+			}
+		}
+	}
+
+	//set next agent
+	return "oldActionCheck();";
+}
+
+?>
--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,3 +1,7 @@
 <?
 define('IBCLOCK_CAT_ID', 2);
+define('IBCLOCK_ACTIONS_ID', 5);
+
+if (file_exists($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/old_actions_agent.php"))
+	include($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/old_actions_agent.php");
 ?>
