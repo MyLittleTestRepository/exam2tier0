f492beeaa82b1927343565a1a2f4f1c4c07e217a
Averin Dmitry <averin@intaro.email>
2018-03-26 10:15:21 +0300

    k2_les5.2 add cache options

--- local/components/custom/list.vacancy/.parameters.php
+++ local/components/custom/list.vacancy/.parameters.php
@@ -6,26 +6,28 @@ if (!CModule::IncludeModule('iblock'))
 
 $arIblockTypes = CIBlockParameters::GetIBlockTypes();
 
-$arIblockIds=[];
+$arIblockIds = [];
 if (isset($arCurrentValues['IBLOCK_TYPE']))
 {
 	$DbRes = CIBlock::GetList('', ["TYPE" => $arCurrentValues['IBLOCK_TYPE'], 'ACTIVE' => 'Y']);
-	while ($iblock=$DbRes->Fetch()){
-		$arIblockIds[$iblock['ID']]='['.$iblock['ID'].'] '.$iblock['NAME'];
+	while ($iblock = $DbRes->Fetch())
+	{
+		$arIblockIds[$iblock['ID']] = '[' . $iblock['ID'] . '] ' . $iblock['NAME'];
 	}
 }
 
 $arComponentParameters = ['GROUPS'     => ["BASE" => ["NAME" => GetMessage('BASE'),
                                                       "SORT" => "10",]],
-                          'PARAMETERS' => ["IBLOCK_TYPE" => ["PARENT"            => "BASE",
-                                                             "NAME"              => GetMessage('IBLOCK_TYPE'),
-                                                             "TYPE"              => "LIST",
-                                                             "REFRESH"           => "Y",
-                                                             "VALUES"            => $arIblockTypes,],
-                                           "IBLOCK_ID" => ["PARENT"            => "BASE",
-                                                             "NAME"              => GetMessage('IBLOCK_ID'),
-                                                             "TYPE"              => "LIST",
-                                                             "VALUES"            => $arIblockIds,],]
+                          'PARAMETERS' => ["IBLOCK_TYPE" => ["PARENT"  => "BASE",
+                                                             "NAME"    => GetMessage('IBLOCK_TYPE'),
+                                                             "TYPE"    => "LIST",
+                                                             "REFRESH" => "Y",
+                                                             "VALUES"  => $arIblockTypes,],
+                                           "IBLOCK_ID"   => ["PARENT" => "BASE",
+                                                             "NAME"   => GetMessage('IBLOCK_ID'),
+                                                             "TYPE"   => "LIST",
+                                                             "VALUES" => $arIblockIds,],
+                                           "CACHE_TIME"  => ["DEFAULT" => 36000000]]
 
 ];
 ?>
